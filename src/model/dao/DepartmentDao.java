292dc66c1a4638f3fa136c348f37846069c1e10f
edsolima <ewerton.silva.o.lima@gmail.com>
2024-02-15 21:04:00 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+
+	void intert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department fingById(Integer id);
+	List<Department> findAll();
+}
