31487174048bcd5d630ba20a54b3c027d6434c78
edsolima <ewerton.silva.o.lima@gmail.com>
2024-02-19 19:53:57 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void intert(Seller obj) {
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+	}
+
+	@Override
+	public Seller fingById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		return null;
+	}
+
+}
