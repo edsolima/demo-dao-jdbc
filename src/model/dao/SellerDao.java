47ff25ca5c3e7ec1180a376441029fed0d0f5ef7
edsolima <ewerton.silva.o.lima@gmail.com>
2024-02-20 20:57:10 -0300

    insert implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,7 +7,7 @@ import model.entities.Seller;
 
 public interface SellerDao {
 
-	void intert(Seller obj);
+	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
 	Seller fingById(Integer id);
